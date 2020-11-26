ba7b2b915f02f7e66505d37201ec4db58a9058de
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-11-26 10:30:21 +0100

    platina: Shim Megvii shared lib
    * Libraries observed from platina-user 10 QKQ1.190910.002 V12.0.3.0.QDTMIXM release-keys
    
    Change-Id: Ie6f0d84f262d01721ba7a66d461fe01e22b66ec2

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libmmcamera_mg_facepp_lib
+
 # Consumerir
 BOARD_HAVE_IR := false
 
